42c19fbb87ef7262bb954ca17c809635058e0c41
Alvaro Antonio <alvaro.ortiz.w0hk@est.uraccan.edu.ni>
2026-06-15 13:00:41 -0400

    first commit

---
+++ src/estudiantes/estudiantes.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { ESTUDIANTES_SERVICE } from 'src/config/service';
+import { CreateEstudianteDto } from './dto/create-estudiante.dto';
+
+@Controller('estudiantes')
+export class EstudiantesController {
+  constructor(
+    @Inject(ESTUDIANTES_SERVICE)
+    private readonly estudianteClient: ClientProxy,
+  ) {}
+
+  @Post()
+  create(@Body() estudianteDto: CreateEstudianteDto) {
+    return this.estudianteClient.send({ cmd: 'create_student' }, estudianteDto);
+  }
+
+  @Get()
+  getAll() {
+    return this.estudianteClient.send({ cmd: 'get_all_student' }, {});
+  }
+
+  @Get(':id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.estudianteClient.send({ cmd: 'get_one_student' }, id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.estudianteClient.send({ cmd: 'remove_student' }, id);
+  }
+}
