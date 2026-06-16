22d4d79bddda211adb5548d16c6bf5ac6a00b3ad
Alvaro Antonio <alvaro.ortiz.w0hk@est.uraccan.edu.ni>
2026-06-16 11:48:11 -0400

    cambios en estudiantes

--- src/estudiantes/estudiantes.module.ts
+++ src/estudiantes/estudiantes.module.ts
@@ -10,8 +10,8 @@ import { ESTUDIANTES_SERVICE } from 'src/config/service';
         name: ESTUDIANTES_SERVICE,
         transport: Transport.TCP,
         options: {
-          host: process.env.ESTUDIANTES_SERVICE_HOST,
-          port: Number(process.env.ESTUDIANTES_SERVICE_PORT),
+          host: process.env.ESTUDIANTES_SERVICES_HOST,
+          port: Number(process.env.ESTUDIANTES_SERVICES_PORT),
         },
       },
     ]),
